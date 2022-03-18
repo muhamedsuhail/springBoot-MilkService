6540e6a8ec15c66e7474184d62ccbb57f0183c4b
muhamedsuhail <muhamed.suhail1@gmail.com>
2022-03-18 17:29:10 +0530

    Initial Project Setup

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Spring Boot MicroServices - Milk Service
+
---
+++ src/main/java/com/demo/milkservice/MilkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.milkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkServiceApplication.class, args);
+	}
+
+}
