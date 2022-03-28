cc820e0741cc1eed174c8df52a9c59f54f217a15
muhamedsuhail <muhamed.suhail1@gmail.com>
2022-03-28 21:54:13 +0530

    Add Bean Validation

--- src/main/java/com/demo/milkservice/repositories/MilkRepository.java
+++ src/main/java/com/demo/milkservice/repositories/MilkRepository.java
@@ -2,8 +2,10 @@ package com.demo.milkservice.repositories;
 
 import com.demo.milkservice.domain.Milk;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface MilkRepository extends PagingAndSortingRepository<Milk,UUID> {
 }
--- src/main/java/com/demo/milkservice/web/model/MilkDTO.java
+++ src/main/java/com/demo/milkservice/web/model/MilkDTO.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,14 +17,23 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MilkDTO {
+
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime modifiedDate;
 
+    @NotBlank
     private String milkName;
 
+    @NotNull
     private MilkTypeEnum milkType;
 
     private Long upc;
